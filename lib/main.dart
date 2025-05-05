9105951541e556a672b06bddc046d4bf4cca3fbc
Majok-Bol <bolmajok001@gmail.com>
2025-05-05 19:13:08 +0300

    added text input field

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:note_flow/pages/home_page.dart';
 void main(){
   runApp(MyApp());
 }
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget{
       home: Scaffold(
         backgroundColor: Colors.grey.shade200,
         appBar: AppBar(title: Text('NoteFlow'),centerTitle: true,backgroundColor: Colors.grey.shade400,),
+        body:HomePage(),
       ),
     );
   }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,1 +1,51 @@
 //handle home page functions
+import 'package:flutter/material.dart';
+class HomePage extends StatefulWidget{
+  @override
+  HomePageState createState()=>HomePageState();
+}
+class HomePageState extends State<HomePage>{
+  //controller
+  final TextEditingController _itemController=TextEditingController();
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+      padding: EdgeInsets.all(12),
+      child: Column(
+        children: [
+          //text field to write notes
+          Expanded(child:TextField(
+            controller: _itemController,
+            expands: true,
+            maxLines:null,
+            minLines: null,
+
+            cursorColor: Colors.black,
+            textAlignVertical: TextAlignVertical.top,
+            decoration:InputDecoration(
+              hintText: 'Write your notes here',
+              contentPadding: EdgeInsets.all(12),
+
+              border:InputBorder.none,
+            ),
+          ))
+          //floating button to add save button
+          // delete notes
+          //view saved notes
+
+        ],
+      ),
+    ),
+        floatingActionButton: FloatingActionButton.extended(onPressed:() {
+
+        },icon:Icon(Icons.save,color: Colors.black,),label: Text('save',style: TextStyle(color: Colors.black,fontSize: 20),),backgroundColor: Colors.blue,),
+      ),
+    );
+
+  }
+
+
+}
